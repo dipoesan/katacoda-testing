2fa651af8a9d8a41f946da71f88422c53addbf13
dipoesan <esan.dipo@gmail.com>
2022-01-16 23:44:49 +0100

    test step 1-a

--- project 3/hint1.sh
+++ project 3/hint1.sh
@@ -6,7 +6,7 @@ seconds_sofar=$1
 
 # echo "Debug Hint Task 2: $seconds_sofar"
 
-# This hint message will appear between 10-20 seconds elapsed after the task began.
+# This hint message will appear between 7-15 seconds elapsed after the task began.
 if [[ $seconds_sofar -ge 7 &&  $seconds_sofar -lt 15 ]]; then
   echo "Still working on it? I could help out if needed..."
 fi
--- project 3/step3.md
+++ project 3/step3.md
@@ -1,5 +1,6 @@
-Which of these commands can you use to download files from the web? <<
-() cat
-() grep
-() pull
+Which of these commands can you use to download files from the web?  <<
+( ) cat
+( ) grep
+( ) pull
 (*) wget
+
